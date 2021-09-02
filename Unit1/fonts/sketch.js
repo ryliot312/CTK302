1fc0751d4eca13dddfd3364a9cd543580d67b119
ryliot312 <rebedel@ilstu.edu>
2021-09-02 11:22:50 -0500

    my fonts

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,15 +1,21 @@
-var f1 ;
+let f1 , f2;
 
 
 
 function setup() {
   createCanvas(500, 500);
   f1 = loadFont("assets/Type_Keys_Filled.ttf") ;
+  f2 = loadFont("assets/Vintage.ttf") ;
+  textAlign(CENTER) ;
 }
 
 function draw() {
   background("yellow") ;
   textFont(f1) ;
   textSize(48) ;
-  text("hello world", 100, 100) ;
+  text("hello world", width/2, 100) ;
+
+  textFont(f2) ;
+  textSizze(24) ;
+  text("Go You Redbirds", width/2, 160) ;
 }
