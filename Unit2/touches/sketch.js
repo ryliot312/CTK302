f641aad0d9427c8ec3237a4c9be2d3669dc35f85
ryliot312 <rebedel@ilstu.edu>
2021-09-28 16:17:37 -0500

    jk

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -34,11 +34,11 @@ function draw() {
       text("three fingers, getting dangerous aren't we", 5, 22);
       break;
 
-    case 4:
-      background('purple');
-      textSize(15);
-      text("four fingers, getting warmer", 5, 22);
-      break;
+    //case 4:
+      //background('purple');
+      //textSize(15);
+    //  text("four fingers, getting warmer", 5, 22);
+    //  break;
 
 
     case 5:
