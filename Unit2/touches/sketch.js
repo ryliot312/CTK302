a3125105a5718c7f0f7bd3f89a3b4587efafef71
ryliot312 <rebedel@ilstu.edu>
2021-09-28 11:20:32 -0500

    new

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -11,22 +11,21 @@ function draw() {
 
   switch(numberOfTouches) {
     case 0:
-    //image()
-      text("no one is touching the screen", 5, 22) ;
+      text("you're not touching the screen", 5, 22) ;
       break ;
 
       case 1:
-       text("it's kind of lonely here", 5, 22) ;
+       text("one finger?", 5, 22) ;
       // put a picture here
       break ;
 
       case 2:
-      text("two fingers are touching the screen", 5, 22) ;
+      text("two fingers, that's better", 5, 22) ;
             // put a picture here
       break ;
 
       case 3:
-     text("three are touching the screen", 5, 22) ;
+     text("three fingers, getting dangerous aren't we", 5, 22) ;
             // put a picture here
       break ;
 
