90b7afa66b202c3e965249a53a3ff6cff3a343a5
ryliot312 <rebedel@ilstu.edu>
2021-10-02 18:23:37 -0500

    song

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,8 +1,17 @@
 let numberOfTouches;
 let image1, image2, image3, image4 ;
+let song1 ;
+
+function preload() {
+  song1 = loadSound("assets/perrysong.mp3");
+
+
+}
 
 function setup() {
   createCanvas(400, 400);
+
+  song1.loop() ;
 
     image1 = loadImage("assets/Baby_Perry_0.jpg") ;
     image2 = loadImage("assets/Perry.jpg") ;
@@ -20,23 +29,35 @@ function draw() {
 
   switch (numberOfTouches) {
     case 0:
-      image(image1, width/2, height/2, 100, 100) ;
+      image(image1, width/2, height/2, 400, 400) ;
       break;
 
     case 1:
-      image(image2, width/2, height/2, 100, 100) ;
+      image(image2, width/2, height/2, 400, 400) ;
       break;
 
     case 2:
-      image(image3, width/2, height/2, 100, 100) ;
+      image(image3, width/2, height/2, 400, 400) ;
       break;
 
     case 3:
-      image(image4, width/2, height/2, 100, 100) ;
+      image(image4, width/2, height/2, 400, 400) ;
       break;
 
 
 
   }
+
+  function mouseReleased() {
+    if (song1.isPlaying()) {
+      song1.pause() ;
+    } else {
+      song1.loop() ;
+    }
+
+  }
+  
+  function touchStarted() {
+    getAudioContext().resume();
 
 }
