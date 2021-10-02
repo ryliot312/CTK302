d06397ffe630c7848192520b52b57b06fc70d3a9
ryliot312 <rebedel@ilstu.edu>
2021-10-02 18:38:07 -0500

    perry2

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,6 +1,6 @@
 let numberOfTouches;
-let image1, image2, image3, image4 ;
-let song1 ;
+let image1, image2, image3, image4;
+let song1;
 
 function preload() {
   song1 = loadSound("sounds/perrysong.mp3");
@@ -11,54 +11,45 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
 
-  song1.loop() ;
+  song1.loop();
 
-    image1 = loadImage("assets/Baby_Perry_0.jpg") ;
-    image2 = loadImage("assets/Perry.jpg") ;
-    image3 = loadImage("assets/agent_p.jpg") ;
-    image4 = loadImage("assets/avenger_perry.jpg") ;
+  image1 = loadImage("assets/Baby_Perry_0.jpg");
+  image2 = loadImage("assets/Perry.jpg");
+  image3 = loadImage("assets/agent_p.jpg");
+  image4 = loadImage("assets/avenger_perry.jpg");
 
 
-     imageMode(CENTER);
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+
 
   switch (numberOfTouches) {
     case 0:
-      image(image1, width/2, height/2, 400, 400) ;
+      image(image1, width / 2, height / 2, 400, 400);
       break;
 
     case 1:
-      image(image2, width/2, height/2, 400, 400) ;
+      image(image2, width / 2, height / 2, 400, 400);
       break;
 
     case 2:
-      image(image3, width/2, height/2, 400, 400) ;
+      image(image3, width / 2, height / 2, 400, 400);
       break;
 
     case 3:
-      image(image4, width/2, height/2, 400, 400) ;
+      image(image4, width / 2, height / 2, 400, 400);
       break;
+}
 
 
 
-  }
-
-  function mouseReleased() {
-    if (song1.isPlaying()) {
-      song1.pause() ;
-    } else {
-      song1.loop() ;
-    }
-
-  }
 
   function touchStarted() {
     getAudioContext().resume();
 
-}
+  }
 }
